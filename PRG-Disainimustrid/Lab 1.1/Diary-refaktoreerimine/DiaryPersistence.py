a88901c07ecafcce8120d2e22260b3ac59dddbff
Nurruw-Meowthra <siimx164@gmail.com>
2026-03-13 11:43:05 +0200

    Add files via upload

---
+++ PRG-Disainimustrid/Lab 1.1/Diary-refaktoreerimine/DiaryPersistence.py
@@ -0,0 +1,19 @@
+from Diary import Diary
+
+class DiaryPersistence:
+
+    @staticmethod
+    def save_to_file(diary, filename):
+        f = open(filename, "w", encoding="utf-8")
+        for entry in diary.entries:
+            f.write(entry + "\n")
+
+
+    @staticmethod
+    def load_from_file(diary, filename):
+        new_diary = Diary()
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = [line.strip() for line in f.readlines()]
+            new_diary.entries = lines
+            new_diary.counter = len(lines)
+        return new_diary
