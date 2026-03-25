c2b1f044b55e00b45cb91494afd3ae3a65e1f480
Siim <siimx164@gmail.com>
2026-03-25 13:27:22 +0200

    Factory pattern (Tehase muster)

---
+++ builder/builder.py
@@ -0,0 +1,23 @@
+class CodeBuilder:
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.fields = []
+
+    def add_field(self, name, value):
+        self.fields.append((name, value))
+        return self
+    
+    def __str__(self):
+        lines = [
+            f"class {self.root_name}:",
+            "  def __init__(self):"
+        ]
+
+        for name, value in self.fields:
+            lines.append(f"    self.{name} = {value}")
+        
+        return "\n".join(lines)
+    
+
+cb = CodeBuilder('Person').add_field('name', '""').add_field('age', '0')
+print(cb)
